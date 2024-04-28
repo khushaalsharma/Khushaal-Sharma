84c6ebeedcabad6cd2ae6009d08a20f39282d5aa
Khushaal Sharma <khushaalsharma1@gmail.com>
2024-04-28 13:10:09 +0530

    new commit - Site fully responsive

--- khushaalsharma/src/Components/Navbar.js
+++ khushaalsharma/src/Components/Navbar.js
@@ -23,23 +23,23 @@ const Navbar = () => {
 
 
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand">
                     <div className='logo-khushaal' onClick={ksClick}>
                         <img src={logo}/>
                     </div>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item" onClick={aboutClick}>
-                            <a class="nav-link" aria-current="page">About</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item" onClick={aboutClick}>
+                            <a className="nav-link" aria-current="page">About</a>
                         </li>
-                        <li class="nav-item" onClick={projectClick}>
-                            <a class="nav-link" aria-current="page">Projects</a>
+                        <li className="nav-item" onClick={projectClick}>
+                            <a className="nav-link" aria-current="page">Projects</a>
                         </li>
                         <li className='nav-item' onClick={contactClick}>
                             <a className='nav-link' aria-current="page">Contact</a>
